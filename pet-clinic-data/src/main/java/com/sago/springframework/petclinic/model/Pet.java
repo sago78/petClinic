a3194e2f11b166d168afd407b4c56209f80e1802
Julio Sanchez <julio-antonio.sanchez@outlook.com>
2020-12-26 00:40:59 -0600

    Added BaseEntity

--- pet-clinic-data/src/main/java/com/sago/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/sago/springframework/petclinic/model/Pet.java
@@ -2,7 +2,7 @@ package com.sago.springframework.petclinic.model;
 
 import java.time.LocalDate;
 
-public class Pet {
+public class Pet extends BaseEntity{
     private PetType petType;
     private String name;
     private Owner owner;
