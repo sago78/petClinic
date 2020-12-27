894d4a76812b9f2b82d7f8183d97c0aab6178b97
Julio Sanchez <julio-antonio.sanchez@outlook.com>
2020-12-26 23:58:47 -0600

    index

--- pet-clinic-web/src/main/java/com/sago/springframework/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/sago/springframework/petclinic/controllers/IndexController.java
@@ -1,13 +1,14 @@
 package com.sago.springframework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
     @RequestMapping({"","/","index","index.html"})
-    public String index(){
+    public String index(Model model){
 
         return "index";
     }
